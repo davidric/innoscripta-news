95fa13c968d6ef98a8481e5040dd67a26802a3eb
davidric <davidlikaldo@gmail.com>
2024-05-05 21:42:54 +0400

    Setup react router config

---
+++ src/pages/Article.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const Article = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Article</p>
+      </header>
+    </div>
+  );
+};
+
+export default Article;
